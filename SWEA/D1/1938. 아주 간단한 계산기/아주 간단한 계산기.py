747fbc56235e32f0e5f674258ac02279d4746778
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-01-13 19:08:28 +0900

    [D1] Title: 아주 간단한 계산기, Time: 155 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
@@ -1,9 +1,6 @@
-import math
 a, b = map(int, input().split())
 if b == 0:
     print("b는 0일 수 없습니다.")
 else :
-    print(a+b)
-    print(a-b)
-    print(a*b)
-    print(math.trunc(a/b))
+    for i in [a+b, a-b, a*b, a//b]:
+        print(i)
