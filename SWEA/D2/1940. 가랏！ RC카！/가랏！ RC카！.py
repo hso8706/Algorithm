0fcf72f2ed5295d025a4f558bc5ab63a9bcf9bce
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-01-16 10:17:41 +0900

    [D2] Title: 가랏! RC카!, Time: 100 ms, Memory: 45,584 KB -BaekjoonHub

---
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -0,0 +1,29 @@
+'''
+Todo
+1. 초기 속도==0 m/s, 최저 속도==0 m/s(0에서 더 감속되더라도 음수가 되진 않는다.)
+2. 입력(a,b)
+    속도 종류 // 0: 현재 속도 유지, 1: 가속, 2: 감속
+    속도 크기
+
+- 문제
+: command에 2개의 숫자가 들어올 때랑, 1개의 숫자(=0)이 들어올 때를 구분하지 못해서 난관 봉착
+'''
+
+T = int(input())
+for test_case in range(1, T+1):
+    sec = int(input())
+    distance = 0
+    vSpeed = 0
+    for s in range(1, sec+1):
+        try:
+            iType, iSpeed = map(int, input().split())
+        except ValueError:
+            iType, iSpeed = 0, 0
+        if iType == 1:
+            vSpeed += iSpeed
+        elif iType == 2:
+            if vSpeed - iSpeed < 0:
+                vSpeed = 0
+            else: vSpeed -= iSpeed
+        distance += vSpeed
+    print("#%d"%test_case, distance)
