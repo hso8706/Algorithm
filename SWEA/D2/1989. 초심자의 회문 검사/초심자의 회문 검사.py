b237eb97cc854ccbbdc724dc9086098041177fa0
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-01-24 21:30:20 +0900

    [D2] Title: 초심자의 회문 검사, Time: 94 ms, Memory: 41,148 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -2,9 +2,7 @@ t = int(input())
 
 for test_case in range(1, t+1):
     char_list = list(input())
-    char_list_reverse = []
-    for i in range(len(char_list)-1, -1, -1):
-        char_list_reverse.append(char_list[i])
+    char_list_reverse = char_list[::-1]
     
     if char_list == char_list_reverse:
         print(f'#{test_case} 1')
