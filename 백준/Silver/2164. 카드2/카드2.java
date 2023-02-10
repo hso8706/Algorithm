88f1702c9850a227a875523a84f9f223d42ee7ce
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-02-10 17:47:13 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45704 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -9,18 +8,18 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(bf.readLine());
-        ArrayDeque<Integer> cards = new ArrayDeque<>();
+        Queue<Integer> cards = new LinkedList<>();
 
         for (int i = 1; i < N + 1; i++) {
             cards.offer(i);
         }
 
         while(cards.size() > 1){
-            cards.pollFirst();
+            cards.poll();
             if (cards.size() == 1) break;
-            cards.addLast(cards.pollFirst());
+            cards.offer(cards.poll());
         }
-        bw.write(cards.pollFirst() + "\n");
+        bw.write(cards.poll() + "\n");
         bw.flush();
         bw.close();
     }
