a16ed94029e65d127228eb285f3af96cc00bd174
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-03-01 19:51:22 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 348 ms, Memory: 18728 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -17,12 +17,14 @@ public class Main {
         - 세로 상황 : 하단, 우하단
         - 대각선 상황 : 우, 우하단, 하단
     3. 첫 시작은 무조건 1,1 에서 1,2
+    + 놓친 부분
+    + 4. 진행 방향에 따라 벽이 무조건 없어야하는 부분이 존재함. 체크할 것
 
     - 목적
     => 1,1 시작으로 N, N 도착 가능한 경우의 수
     
     - 문제 해결
-        - dfs 선택: N, N에 도달하면 cnt++ && 처음부터 다시 시작
+        - dfs 선택: N, N에 도달하면 cnt++ && 방문 여부 체크 false로 변경하여 다시 회귀
 
      */
     static int N; // 집의 크기
@@ -64,10 +66,10 @@ public class Main {
                     int nx = x + dx[i];
                     int ny = y + dy[i];
                     if(nx<=0 || nx > N || ny <=0 || ny > N) continue;
-                    if(isWall(i, y, x)) continue;
+                    if(isWall(i, y, x)) continue; // 상황별 벽 대응
                     if(!visited[ny][nx]){
                         dfs(ny, nx, i);
-                        visited[ny][nx] = false;
+                        visited[ny][nx] = false; // 이전 분기점으로 회귀하면서 재방문을 위한 false 변경
                     }
                 }
                 break;
@@ -100,6 +102,8 @@ public class Main {
         }
     }
 
+    //체크사항 4.
+    //해당 위치에 벽이 있으면 true 반환하여 위 반복문 continue;
     private static boolean isWall(int nextD, int y, int x) {
         switch (nextD){
             // 가로 진행 방향
