1f5852a1a545be34c800f91eb1652af5e1420de8
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-04-19 10:25:26 +0900

    [Silver II] Title: 부분수열의 합, Time: 200 ms, Memory: 41928 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,16 +13,13 @@ public class Main {
      - 수열 중 부분 수열의 합이 특정한 값(S)이 되는 경우 구하기
 
      ### 해결1.
-     - 모든 부분 집합을 구하고, 각각의 부분 집합의 원소의 합을 구해서 S와 비교
-        - 2초, 256MB, N<=20(2^20 == 대충 100만) 가능할 듯
-
-     ### 해결2.
+     - 이전 해결: 모든 부분 집합을 구하고, 각각의 부분 집합의 원소의 합을 구해서 S와 비교
+        - 불가, 조건이 타이트해짐
      */
     static int N, S;
     static int[] inputArr;
-    static boolean[] isSelected;
-    static int[] groupS;
     static int totalCnt;
+
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(bf.readLine());
         N = Integer.parseInt(st.nextToken());
@@ -31,39 +30,34 @@ public class Main {
         for (int i = 0; i < N; i++) {
             inputArr[i] = Integer.parseInt(st.nextToken());
         }
-        isSelected = new boolean[N];
-        subset(0);
+
+        sumSubset();
+
         bw.write(totalCnt + "");
         bw.flush();
         bw.close();
     }
 
-    private static void subset(int cnt) throws IOException {
-        if(cnt == N){
-            groupS = new int[N];
-            int checkTF = 0;
-            for (int i = 0; i < N; i++) {
-                if(isSelected[i]) {
-                    groupS[i] = inputArr[i];
-                    checkTF++;
-                }
-            }
-            if(checkTF == 0) return;
-            if(checkS()) totalCnt++;
-            return;
-        }
-
-        isSelected[cnt] = true;
-        subset(cnt+1);
-        isSelected[cnt] = false;
-        subset(cnt+1);
-    }
-
-    private static boolean checkS() {
-        int sum = 0;
+    private static void sumSubset() {
+        Queue<int[]> queue = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
-            sum += groupS[i];
+            queue.offer(new int[]{i, inputArr[i]});
         }
-        return sum == S;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int ci = current[0];
+            int cs = current[1];
+            if(cs == S){
+                totalCnt++;
+            }
+
+            for (int i = ci+1; i < N; i++) {
+                int ni = i;
+                int ns = cs + inputArr[ni];
+
+                queue.offer(new int[]{ni,ns});
+            }
+        }
     }
 }
