9df5abc04bafc6393c2e83966e646d536e2c2791
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-04-28 08:17:44 +0900

    [Silver IV] Title: 수들의 합 2, Time: 200 ms, Memory: 15160 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+
+    /*
+    수열 중 연속된 인덱스의 합이 M이 되는 경우의 수 출력
+    
+    1. i 번째부터 j 번째까지 합을 구해서 비교해보기
+        - 무조건 안 될 것 같긴한데
+     */
+    static int N, M; // N: 수열 개수, M: 총합 목표
+    static int[] arr;
+    static int cnt;
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = i; j < N; j++) {
+                sum += arr[j];
+                if(sum == M) {
+                    cnt++;
+                    break;
+                }
+                if(sum > M) break;;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
