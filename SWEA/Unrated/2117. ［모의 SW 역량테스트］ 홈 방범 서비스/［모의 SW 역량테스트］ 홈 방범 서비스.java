5b7f665101c4be1fc4dac72dea60d387df1760e5
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-05-16 20:21:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 홈 방범 서비스, Time: 305 ms, Memory: 24,716 KB -BaekjoonHub

--- SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
+++ SWEA/Unrated/2117. ［모의 SW 역량테스트］ 홈 방범 서비스/［모의 SW 역량테스트］ 홈 방범 서비스.java
@@ -1,113 +1,77 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-// k (서비스의 길이) 가 작은 ~~ 큰 범위로 경우의 수를 따진다. <= 어느 특정 지점에서 어느 크기의 서비스 영역을 정하느냐에 따라
-//   비용, 집의 수가 각각 달라진다. => 특정지을 수 없다. 어느좌표가 유리한지. 어느 k 가 유리한지....
-// 완탐!
-// 모든 좌표에서 모든 k를 다 고려
-//    1. map 2차원 배열 순회, 탐색 X
-//		=> 어차피 k 값은 순차적으로 증가, 또는 감소하는 static 한 값
-//	    => 집의 위치가 알 수 없는 미궁의??  => 입력처리를 하면서 집을 별도의 자료구조에 저장 List 에 (좌표)
-//    2. map 2차원 배열 순회, 탐색 O
-//		=> 모든 좌표에서 서비스를 모든 k에 대해서 진행하되 k의 길이만큼 맵에서 가보는 것.
-
-// 2번 2차원 배열 순회 탐색
 public class Solution {
 
-    static int T, N, M, ans;
-    static int[][] map;
-    static int[] cost;
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+    /*
+    - 마름모 꼴 서비스 영역
+    - 운영 비용 계산식
+    - 시작 지점을 특정할 수 없음 => 어느 지점이 유리한 지 모르기 때문
+        - 완탐을 시행해야한다.
+
+    - 완탐
+        - 모든 좌표에서 모든 k를 고려
+        - 1.map 2차원 배열 순회, 탐색 X
+            - 별도의 자료구조에 집 좌표를 관리
+        - 2.map 2차원 배열 순회, 탐색 O
+     */
+    static int T, N, K, M, ans;
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(bf.readLine());
 
         for (int t = 1; t <= T; t++) {
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            bw.write("#"+t+" ");
+            st = new StringTokenizer(bf.readLine());
             N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken()); // 집당 수익
-
-            map = new int[N][N];
+            M = Integer.parseInt(st.nextToken());
+            List<int[]> house = new ArrayList<>(); // 집 리스트
 
             for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine());
+                st = new StringTokenizer(bf.readLine());
                 for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+                    int num = Integer.parseInt(st.nextToken());
+                    if(num == 1) house.add(new int[] {i,j});
                 }
             }
-            // 입력 완료
 
+            int size = house.size();
             ans = 0;
+            
+            // K 서비스 길이는 1 ~ 얼마까지?
+            // K = N % 2 == 0 ? N+1 : N; => 내부 가장 큰 영역에 해당되는 상황을 그리면서 설명해주심
+            K = N % 2 == 0 ? N+1 : N;
 
-            // K 서비스의 길이는 1 ~ 얼마까지?
-            // N이 홀수 : N과 동일, N이 짝수 : N + 1
-            // cost[0] : dummy cost[1] : k == 1 비용, cost[N+1]까지 계산
-            cost = new int[N + 2];
+            for (int k = 1; k <= K; k++) { // 서비스 영역을 미리 만든 후 시작 지점을 달리하며 체크 => x,y 반복문을 먼저 돌려도 무관
+                int cost = k*k + (k-1)*(k-1);
 
-            for (int k = 1; k <= N + 1; k++) {
-                cost[k] = k*k + (k-1)*(k-1);
-            }
+                for (int y = 0; y < N; y++) {
+                    for (int x = 0; x < N; x++) {
 
-            // 2차원 배열을 돌면서 모든 지점에 서비스를 놓는 경우를 따진다.
-            for (int y = 0; y < N; y++) {
-                for (int x = 0; x < N; x++) {
+                        int cnt = 0; // 서비스에 포함된 집의 수
+                        //모든 집에 대해서 체크
+                        for (int i = 0; i < size; i++) { // 서비스에 포함된 집의 수
+                            if( (Math.abs( house.get(i)[0] - y) + Math.abs( house.get(i)[1] - x)) < k) cnt++;
+                        }
 
-                    check(y, x);
+                        //비용과 이익, 집의 수
+                        if(cnt * M >= cost) ans = Math.max(ans, cnt);
+                    }
                 }
             }
-
-
-            sb.append("#").append(t).append(" ").append(ans).append("\n");
+            bw.write(ans + "\n");
         }
-
-        System.out.println(sb);
-    }
-
-    static void check(int y, int x) {
-        // 동일한 y, x 좌표에서 k 를 1부터 쭉 따져나가는데 이전에 따진 내용을 재활용
-        int[] cnt = new int[N + 2]; // cnt[3]
-        for (int k = 1; k <= N + 1; k++) {
-            cnt[k] = countBoundaryHouse(y, x, k) + cnt[k-1];
-            if( cnt[k]*M >= cost[k] ) ans = Math.max(ans, cnt[k]);
-        }
-    }
-
-    static int[] dy = { -1, 1, 0, 0 };
-    static int[] dx = {  0, 0,-1, 1 };
-
-    static int[] dyBorder = { 1, 1, -1, -1 };
-    static int[] dxBorder = { 1,-1, -1,  1 };
-
-    static int countBoundaryHouse(int y, int x, int k) {
-        int cnt = 0;
-        if( k == 1 ) cnt = map[y][x] == 1 ? 1 : 0;
-        else if( k == 2 ) {
-            for (int d = 0; d < 4; d++) {
-                int ny = y + dy[d];
-                int nx = x + dx[d];
-
-                if( ny < 0 || nx < 0 || ny >= N || nx >= N ) continue;
-                if( map[ny][nx] == 1 ) cnt++;
-            }
-        }else {
-            // 꼭대기 보정
-            int ny = y - k + 1;
-            int nx = x;
-
-            // 꼭대기 좌표부터 외각의 마름모를 따라가면서 집확인
-            for (int d = 0; d < 4; d++) {
-                for (int i = 0; i < k - 1; i++) {
-                    ny = ny + dyBorder[d];
-                    nx = nx + dxBorder[d];
-
-                    if( ny < 0 || nx < 0 || ny >= N || nx >= N ) continue;
-                    if( map[ny][nx] == 1 ) cnt++;
-                }
-            }
-        }
-        return cnt;
+        bw.flush();
+        bw.close();
     }
 }
