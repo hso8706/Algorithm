31ecb5d206c80a23111fe2cfdbb0a8c0970d2b34
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-11-22 14:22:44 +0900

    [Gold IV] Title: 알파벳, Time: 2720 ms, Memory: 20620 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,61 +1,70 @@
-import java.io.*;
-    import java.lang.reflect.Array;
-    import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-    public class Main {
-        static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        static StringTokenizer st;
-        static StringBuilder sb;
+public class Main {
+    static BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
 
-        static int R, C; //R: 세로, C: 가로
-        static String[][] board; //R x C
-        static String visited;
-        static ArrayList<Integer> move;
-        static int maxRoute;
-        static int[] dx = new int[]{-1, 0, 1, 0};//상우하좌
-        static int[] dy = new int[]{0, 1, 0, -1};
+    static int R, C;
+    static char[][] map;
+    static boolean[][] visited;
+//    static ArrayList<Character> route;
+    static Deque<Character> route;
+    static int steps;
+    static int[] dr = new int[]{-1,0,1,0};
+    static int[] dc = new int[]{0,1,0,-1};
 
-        public static void main(String[] args) throws IOException {
-            st = new StringTokenizer(bf.readLine());
-            R = Integer.parseInt(st.nextToken());
-            C = Integer.parseInt(st.nextToken());
-            board = new String[R][C];
-            visited = "";
-            maxRoute = Integer.MIN_VALUE;
-            sb = new StringBuilder();
-            move = new ArrayList<>();
-            for (int i = 0; i < R; i++) {
-                String[] temp = bf.readLine().split("");
-                for (int j = 0; j < C; j++) {
-                    board[i][j] = temp[j];
-                }
+    public static void main(String[] args) throws IOException {
+
+        init();
+        visited[1][1] = true;
+        route.offerFirst(map[1][1]);
+        dfs(1,1,new boolean[R+1][C+1]);
+
+        System.out.println(steps);
+    }
+
+    private static void dfs(int r, int c, boolean[][] visited) {
+        steps = Math.max(steps, route.size());
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(nr<1 || nc<1 || nr>R || nc>C) continue;
+            if(!visited[nr][nc] && !route.contains(map[nr][nc])){
+                route.offerFirst(map[nr][nc]);
+                visited[nr][nc] = true;
+                dfs(nr,nc, visited);
+                route.pollFirst();
+                visited[nr][nc] = false;
             }
-            canMove(0, 0);
-            move.sort(Comparator.reverseOrder());
-//            bw.write(move.get(0)+"");
-            bw.write(maxRoute+"");
-            bw.flush();
-            bw.close();
-        }
-
-        private static void canMove(int x, int y) {
-            if(sb.toString().contains(board[x][y])) {
-//                move.add(sb.length());
-                if(maxRoute < sb.length()) maxRoute = sb.length();
-                return;
-            }
-
-//            visited += board[x][y];
-            sb.append(board[x][y]);
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-                canMove(nx, ny);
-            }
-//            visited = visited.substring(0, visited.length()-1);
-            sb.deleteCharAt(sb.length()-1);
         }
     }
+
+    private static void init() throws IOException {
+
+        st = new StringTokenizer(bf.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R+1][C+1];
+
+        for (int i = 1; i < R+1; i++) {
+            char[] inputs = bf.readLine().toCharArray();
+            for (int j = 1; j < C+1; j++) {
+                map[i][j] = inputs[j-1];
+            }
+        }
+        visited = new boolean[R+1][C+1];
+//        route = new ArrayList<>();
+        route = new ArrayDeque<>();
+        steps = 0;
+    }
+
+}
