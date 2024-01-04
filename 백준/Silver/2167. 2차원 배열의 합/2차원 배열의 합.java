e05b5545f883393eb318a16734d73f5a9852b988
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2024-01-04 14:46:34 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 392 ms, Memory: 28864 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -6,29 +10,16 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     static StringTokenizer st;
 
-    /*
-    ### 2차원 배열의 합
-    - A(i,j)와 B(x,y)의 관계
-        - A랑 B는 같은 row, col 에 있을 수 있음
-        - A는 좌측 상단, B는 우측 하단이 기본
-            - 즉, col: i<=x && row: j<=y 를 구해주면 된다.
-     */
     static int N, M, K;
-    static int[][] secDim;
+    static int[][] map;
+    static int[][] dp;
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(bf.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        secDim = new int[N+1][M+1];
 
-        for (int i = 1; i < N+1; i++) {
-            st = new StringTokenizer(bf.readLine());
-            for (int j = 1; j < M+1; j++) {
-                secDim[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+        init();
+        findSum();
+    }
 
-        K = Integer.parseInt(bf.readLine());
+    private static void findSum() throws IOException {
         for (int n = 0; n < K; n++) {
             st = new StringTokenizer(bf.readLine());
             int i = Integer.parseInt(st.nextToken());
@@ -36,20 +27,30 @@ public class Main {
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            int sum = sumOfPart(i,j,x,y);
+            int sum = 0;
+            for (int n1 = i; n1 <= x; n1++) {
+                sum += dp[n1][y] - dp[n1][j-1];
+            }
             bw.write(sum+"\n");
         }
         bw.flush();
         bw.close();
     }
 
-    private static int sumOfPart(int i, int j, int x, int y) {
-        int s = 0;
-        for (int row = i; row <= x; row++) {
-            for (int col = j; col <= y ; col++) {
-                s += secDim[row][col];
+    private static void init() throws IOException{
+
+        st = new StringTokenizer(bf.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N+1][M+1];
+        dp = new int[N+1][M+1];
+        for (int i = 1; i < N+1; i++) {
+            st = new StringTokenizer(bf.readLine());
+            for (int j = 1; j < M+1; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = map[i][j] + dp[i][j-1];
             }
         }
-        return s;
+        K = Integer.parseInt(bf.readLine());
     }
 }
